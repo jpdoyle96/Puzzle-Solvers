bbffe0f5a4ca6719bfbdfd6e64163b9315648c04
jpdoyle96 <jpdoyle96@gmail.com>
2024-02-20 13:45:35 -0500

    Added first puzzle to ripple effect

---
+++ RippleEffect/main.py
@@ -0,0 +1,15 @@
+# Ripple Effect
+# 
+# Header here
+
+
+def solve_puzzle(args):
+    print("UNIMPLEMENTED")
+
+def create_puzzle(args):
+    print("UNIMPLEMENTED")
+
+# Systems args here
+
+# Main functionality here as well
+
